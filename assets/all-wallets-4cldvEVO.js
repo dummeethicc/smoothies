e6db772e5000ba477cb27dd21f5d44ed6411977d
aibowtf <sora@aibo.wtf>
2025-10-09 13:55:04 -0700

    Updates

--- assets/all-wallets-4cldvEVO.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-Dv3BlXzm.js";import"./index-Mr_r3L0G.js";import"./index.es-CiDNs6bW.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{p as allWalletsSvg};
